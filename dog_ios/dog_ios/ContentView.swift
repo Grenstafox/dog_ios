b91c188fa397e09f9c6fec3dba74e8def8b8427c
alumno <alumno@lap-fpizarro.uacj.mx>
2025-05-19 10:46:30 -0600

    Estudia un poco mas

--- dog_ios/dog_ios/ContentView.swift
+++ dog_ios/dog_ios/ContentView.swift
@@ -1,20 +1,61 @@
 //
 //  ContentView.swift
-//  dog_ios
+//  bocetos_IOS
 //
-//  Created by alumno on 5/16/25.
+//  Created by alumno on 4/7/25.
 //
 
 import SwiftUI
 
+import PhotosUI
+
 struct ContentView: View {
+    @State var foto_seleccionada:
+        PhotosPickerItem? = nil
+    @State var foto_a_mostrar: UIImage? = nil
+    
     var body: some View {
+        ZStack{
+            Rectangle()
+                .foregroundColor(tema_colores["color_2"])
+                .cornerRadius(40)
+                .frame(width: 400, height: 100)
+            Text("Hola a APIDOG")
+                .font(Font.custom("",size: 50))
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+        }      .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Rectangle()
+                .foregroundColor(.yellow)
+                .cornerRadius(40)
+                .frame(width: 250, height: 250)
+            PhotosPicker(selection: $foto_seleccionada){
+                Image(uiImage: foto_a_mostrar ?? UIImage(resource: .avatar))
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150, height: 150)
+                    .clipShape(.circle)
+            }
+        }
+   
+        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+        .onChange(of: foto_seleccionada) {valor_anterior, valor_nuevo in
+            Task{
+                if let foto_seleccionada, let datos = try? await
+                    foto_seleccionada.loadTransferable(type: Data.self){
+                    if let image = UIImage(data: datos){
+                        foto_a_mostrar = image
+                    }
+                }
+            }
+        }
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-        }
+            
+        }      .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
         .padding()
     }
 }
---
+++ dog_ios/dog_ios/recursos/colores.swift
@@ -0,0 +1,14 @@
+//
+//  colores.swift
+//  dog_ios
+//
+//  Created by alumno on 5/19/25.
+//
+
+import Foundation
+import SwiftUI
+
+let tema_colores = [
+    "color_1" : Color(red: 0.89, green: 0.65, blue: 0.22),
+    "color_2": Color(red: 0.80, green: 0.65, blue: 0.22)
+]
